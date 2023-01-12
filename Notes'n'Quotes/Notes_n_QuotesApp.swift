93f3f47da5350f2e4f295a3d83c652d9b723152a
Isaac Hamm <isaachamm4@gmail.com>
2023-01-12 14:39:39 -0700

    Initial Commit

---
+++ Notes'n'Quotes/Notes_n_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_n_QuotesApp.swift
+//  Notes'n'Quotes
+//
+//  Created by Isaac Hamm on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_n_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
